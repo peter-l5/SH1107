d34f8b38346b3e67b66a7d7b4ac311aebeb98210
peter-l5 <105649844+peter-l5@users.noreply.github.com>
2023-05-13 17:24:26 +0100

    v317 128x64 displays enhancement
    add support for 128x64 displays and micropython v1.20.0 methods

--- sh1107.py
+++ sh1107.py
@@ -1,7 +1,6 @@
-#
 # MicroPython SH1107 OLED driver, I2C interfaces
 # tested with Raspberry Pi Pico and adafruit 1.12 inch QWIC OLED display
-# sh1107 driver v311
+# sh1107 driver v317
 #
 # The MIT License (MIT)
 #
@@ -73,31 +72,20 @@
 # display.text('driver', 0, 8, 1)
 # display.show()
 
-__version__ = "v311"
+__version__ = "v317"
 __repo__ = "https://github.com/peter-l5/SH1107"
 
 ## SH1107 module code
 from micropython import const
-import time #as time
-# line below is in preparation for possible framebuffer extensions
+import time
+# import extended framebuffer if available)
 try:
     import framebuf2 as framebuf
     _fb_variant = 2
 except:
     import framebuf
     _fb_variant = 1
-print('framebuf is ', ('standard' if _fb_variant ==1 else 'extended') )
-
-#define timed_function decorator
-def timed_function(f, *args, **kwargs):
-    myname = str(f).split(' ')[1]
-    def new_func(*args, **kwargs):
-        t = time.ticks_us()
-        result = f(*args, **kwargs)
-        delta = time.ticks_diff(time.ticks_us(), t)
-        print('Function {} Time = {:7.3f}ms'.format(myname, delta/1000))
-        return result
-    return new_func
+print("framebuf is ", ("standard" if _fb_variant ==1 else "extended") )
 
 # a few register definitions with SH1107 data sheet reference numbers
 _LOW_COLUMN_ADDRESS      = const(0x00)   # 1. Set Column Address 4 lower bits (POR = 00H) 
@@ -106,7 +94,7 @@ _MEM_ADDRESSING_MODE     = const(0x20)   # 3. Set Memory addressing mode
                                          #    0x20 horizontal addressing; 0x21 vertical addressing
 _SET_CONTRAST            = const(0x8100) # 4. Set Contrast Control (double byte command)
 _SET_SEGMENT_REMAP       = const(0xa0)   # 5. Set Segment Re-map: (A0H - A1H)
-_SET_MULTIPLEX_RATIO     = const(0xA87F) # 6. Set Multiplex Ratio: (Double Bytes Command)
+_SET_MULTIPLEX_RATIO     = const(0xA800) # 6. Set Multiplex Ratio: (Double Bytes Command)
                                          #    duty = 1/64 [3f]  or 128 [7f] (POR)
 _SET_NORMAL_INVERSE      = const(0xa6)   # 8. Set Normal/Reverse Display: (A6H -A7H)
 _SET_DISPLAY_OFFSET      = const(0xD300) # 9. Set Display Offset: (Double Bytes Command)
@@ -119,7 +107,7 @@ _SET_DC_DC_CONVERTER_SF  = const(0xad8d) # 10. Set DC-DC Setting (set charge pum
 _SET_DISPLAY_OFF         = const(0xae)   # 11. Display OFF/ON: (AEH - AFH)
 _SET_DISPLAY_ON          = const(0xaf)   # 11. Display OFF/ON: (AEH - AFH)
 _SET_PAGE_ADDRESS        = const(0xB0)   # 12. Set Page Address (using 4 low bits)
-_SET_SCAN_DIRECTION      = const(0xc0)   # 13. Set Common Output Scan Direction: (C0H - C8H)
+_SET_SCAN_DIRECTION      = const(0xC0)   # 13. Set Common Output Scan Direction: (C0H - C8H)
 _SET_DISPLAY_START_LINE  = const(0xDC00) # 17. Set Display Start Line (double byte command)
 
 
@@ -129,49 +117,49 @@ class SH1107(framebuf.FrameBuffer):
         self.width = width
         self.height = height
         self.external_vcc = external_vcc
-        self.flip_flag = rotate == 180 or rotate == 90
+        self.flip_flag = False 
         self.rotate90 = rotate == 90 or rotate == 270
+        self.rotate = rotate
         self.inverse = False
+        if self.rotate90:
+            self.width, self.height = self.height, self.width
         self.pages = self.height // 8
         self.row_width = self.width // 8
         self.bufsize = self.pages * self.width
-        self.renderbuf = bytearray(self.bufsize)
-        self.renderbuf_mv = memoryview(self.renderbuf)
+        self.displaybuf = bytearray(self.bufsize)
+        self.displaybuf_mv = memoryview(self.displaybuf)
         self.pages_to_update = 0
         self._is_awake = False
         if self.rotate90:
-            # HMSB is required to keep the bit order in the render buffer
-            # compatible with byte-for-byte remapping to the display's memory,
-            # which is in VLSB. 
-            super().__init__(self.renderbuf, self.height, self.width,
-                             framebuf.MONO_HMSB)
-        else:
-            super().__init__(self.renderbuf, self.width, self.height,
+            super().__init__(self.displaybuf, self.width, self.height,
                              framebuf.MONO_VLSB)
+        else:
+            super().__init__(self.displaybuf, self.width, self.height, 
+                             framebuf.MONO_HMSB)
         self.init_display()
 
     def init_display(self):
+        multiplex_ratio = 0x7F if (self.height == 128)  else 0x3F
         self.reset()
-        self.poweroff()  #turn off OLED display
+        self.poweroff()
         self.fill(0)
-        self.write_command( _SET_DC_DC_CONVERTER_SF.to_bytes(2,'big') )
-        self.write_command( _SET_MULTIPLEX_RATIO.to_bytes(2,'big') ) 
-        self.write_command( _SET_DISPLAY_OFFSET.to_bytes(2,'big') )
-        self.write_command( (_MEM_ADDRESSING_MODE | (0x01 if self.rotate90 else 0x00) ).to_bytes(1,'big') )
-        self.write_command( _SET_PAGE_ADDRESS.to_bytes(1,'big') ) # set page address to zero
-        self.contrast(0) # set display to low contrast
-        self.invert(0)   # normal (not inverse) display
-        # requires a call to flip() for setting up.
+        self.write_command(_SET_DC_DC_CONVERTER_SF.to_bytes(2,"big"))
+        self.write_command((_SET_MULTIPLEX_RATIO | multiplex_ratio).to_bytes(2,"big"))
+        self.write_command((_MEM_ADDRESSING_MODE | (0x00 if self.rotate90 else 0x01)).to_bytes(1,"big"))
+        self.write_command(_SET_PAGE_ADDRESS.to_bytes(1,"big")) # set page address to zero
+        self.contrast(0)
+        self.invert(0)
+        # requires a call to flip() for setting up
         self.flip(self.flip_flag)
         self.poweron()
 
     def poweron(self):
-        self.write_command(_SET_DISPLAY_ON.to_bytes(1,'big'))
+        self.write_command(_SET_DISPLAY_ON.to_bytes(1,"big"))
         self._is_awake = True
         time.sleep_ms(100) # SH1107 recommended delay in power on sequence
         
     def poweroff(self):
-        self.write_command(_SET_DISPLAY_OFF.to_bytes(1,'big'))
+        self.write_command(_SET_DISPLAY_OFF.to_bytes(1,"big"))
         self._is_awake = False
 
     def sleep(self, value=True):
@@ -187,70 +175,74 @@ class SH1107(framebuf.FrameBuffer):
     def flip(self, flag=None, update=True):
         if flag is None:
             flag = not self.flip_flag
-        mirror_v = flag ^ self.rotate90
-        mirror_h =  flag 
-        self.write_command( (_SET_SEGMENT_REMAP   | (0x01 if mirror_v else 0x00) ).to_bytes(1,'big') )
-        self.write_command( (_SET_SCAN_DIRECTION  | (0x08 if mirror_h else 0x00) ).to_bytes(1,'big') )
+        if self.height == 128 and self.width == 128:
+            row_offset = 0x00
+        elif self.rotate90:
+            row_offset = 0x60
+        else:
+            row_offset = 0x20 if (self.rotate == 180) ^ flag else 0x60
+        remap = 0x00 if (self.rotate in (90, 180)) ^ flag else 0x01 
+        direction = 0x08 if (self.rotate in (180, 270)) ^ flag else 0x00
+        self.write_command((_SET_DISPLAY_OFFSET | row_offset).to_bytes(2,"big"))
+        self.write_command((_SET_SEGMENT_REMAP  | remap ).to_bytes(1,"big") )
+        self.write_command((_SET_SCAN_DIRECTION | direction ).to_bytes(1,"big") )
         self.flip_flag = flag
         if update:
             self.show(True) # full update
 
     def display_start_line(self, value):
-        # 17. Set Display Start Line:（Double Bytes Command）
-        # valid values are 0 (Power on /Reset) to 127 (x00-x7F)
-        self.write_command( (_SET_DISPLAY_START_LINE | (value & 0x7F)).to_bytes(2,'big') )
+        """
+        17. Set Display Start Line:（Double Bytes Command）
+        valid values are 0 (Power on /Reset) to 127 (x00-x7F)
+        """
+        self.write_command((_SET_DISPLAY_START_LINE | (value & 0x7F)).to_bytes(2,"big"))
         
     def contrast(self, contrast):
-        # 4. contrast can be between 0 (low), 0x80 (POR) and 0xff (high)
-        # the segment current increases with higher values
-        self.write_command( (_SET_CONTRAST | (contrast & 0xFF)).to_bytes(2,'big') )
+        """
+        4. contrast can be between 0 (low), 0x80 (POR) and 0xff (high)
+        the segment current increases with higher values
+        """
+        self.write_command((_SET_CONTRAST | (contrast & 0xFF)).to_bytes(2,"big"))
 
     def invert(self, invert=None):
         if invert == None:
             invert = not self.inverse
-        self.write_command( (_SET_NORMAL_INVERSE | (invert & 1)).to_bytes(1,'big')  )
+        self.write_command((_SET_NORMAL_INVERSE | (invert & 1)).to_bytes(1,"big"))
         self.inverse = invert
-    
-#     @timed_function
+
     def show(self, full_update: bool = False):
-#         _start = time.ticks_us() 
-        (w, p, rb_mv) = (self.width, self.pages, self.renderbuf_mv)
-        commandbuffer = bytearray(3)
-        commandbuffer2 = bytearray(2)
+#         _start = time.ticks_us()
+        (w, p, db_mv) = (self.width, self.pages, self.displaybuf_mv)
         current_page = 1
         if full_update:
             pages_to_update = (1 << p) - 1
         else:
             pages_to_update = self.pages_to_update
         if self.rotate90:
-            row_bytes = w // 8
-            for start_row in range(0, p*8, 8):
-                # tell display which row
-                if (pages_to_update & current_page):
-                    # update 8 display rows
-                    for row in range(start_row, start_row + 8):
-                        # set row address for update 
-                        # sending 2 commands to the display controller in one i2c write
-                        # rather than 2 separate writes speeds up the refresh   
-                        commandbuffer2[0] = row & 0x0f  # low column write address (low col. cmd is 0x00)
-                        commandbuffer2[1] = _HIGH_COLUMN_ADDRESS | (row >> 4) # high column write address
-                        self.write_command(commandbuffer2)
-                        # take a row of data from the screen framebuffer and write to the display
-                        slice_start = row * row_bytes
-                        self.write_data(rb_mv[slice_start:(slice_start + row_bytes)])
+            buffer_3Bytes = bytearray(3)
+            buffer_3Bytes[1] = _LOW_COLUMN_ADDRESS
+            buffer_3Bytes[2] = _HIGH_COLUMN_ADDRESS
+            for page in range(p):
+                if pages_to_update & current_page:
+                    buffer_3Bytes[0] = _SET_PAGE_ADDRESS | page
+                    self.write_command(buffer_3Bytes)
+                    page_start = w * page
+                    self.write_data(db_mv[page_start : page_start + w])
                 current_page <<= 1
         else:
-            for page in range(p):
-                if (pages_to_update & current_page):
-                    commandbuffer[0] = _SET_PAGE_ADDRESS | page
-                    commandbuffer[1] = _LOW_COLUMN_ADDRESS
-                    commandbuffer[2] = _HIGH_COLUMN_ADDRESS
-                    self.write_command(commandbuffer)
-                    page_start = w * page
-                    self.write_data(rb_mv [(page_start):(page_start+w)])
+            row_bytes = w // 8
+            buffer_2Bytes = bytearray(2)
+            for start_row in range(0, p * 8, 8):
+                if pages_to_update & current_page:
+                    for row in range(start_row, start_row + 8):
+                        buffer_2Bytes[0] = row & 0x0f  # low column (low col. cmd is 0x00)
+                        buffer_2Bytes[1] = _HIGH_COLUMN_ADDRESS | (row >> 4) 
+                        self.write_command(buffer_2Bytes)
+                        slice_start = row * row_bytes
+                        self.write_data(db_mv[slice_start : slice_start + row_bytes])
                 current_page <<= 1
         self.pages_to_update = 0
-#         print('screen update used ', (time.ticks_us()- _start)/1000,'ms')
+#         print("screen update used ", (time.ticks_us() - _start) / 1000, "ms")
 
     def pixel(self, x, y, c=None):
         if c is None:
@@ -262,7 +254,7 @@ class SH1107(framebuf.FrameBuffer):
 
     def text(self, text, x, y, c=1):
         super().text(text, x, y, c)
-        self.register_updates(y, y+7)
+        self.register_updates(y, y + 7)
 
     def line(self, x0, y0, x1, y1, c):
         super().line(x0, y0, x1, y1, c)
@@ -274,7 +266,7 @@ class SH1107(framebuf.FrameBuffer):
 
     def vline(self, x, y, h, c):
         super().vline(x, y, h, c)
-        self.register_updates(y, y+h-1)
+        self.register_updates(y, y + h - 1)
 
     def fill(self, c):
         super().fill(c)
@@ -282,54 +274,60 @@ class SH1107(framebuf.FrameBuffer):
 
     def blit(self, fbuf, x, y, key=-1, palette=None):
         super().blit(fbuf, x, y, key, palette)
-        self.register_updates(y, y+self.height)
+        self.register_updates(y, y + self.height)
 
     def scroll(self, x, y):
         # my understanding is that scroll() does a full screen change
         super().scroll(x, y)
-        self.pages_to_update =  (1 << self.pages) - 1
+        self.pages_to_update = (1 << self.pages) - 1
 
     # rect() and fill_rect() amended to be compatible with new rect() method
-    # from latest micropython as well as 1.19.1 and previous versions
+    # from latest micropython as well as 1.20.0 and previous versions
     def fill_rect(self, x, y, w, h, c):
         try:
             super().fill_rect(x, y, w, h, c)
         except:
             super().rect(x, y, w, h, c, f=True)
-        self.register_updates(y, y+h-1)
+        self.register_updates(y, y + h - 1)
 
     def rect(self, x, y, w, h, c, f=None):
-        if f == None:
-            super().rect(x, y, w, h, c)
-        elif f == False:
+        if f == None or f == False:
             super().rect(x, y, w, h, c)
         else:
             try:
                 super().rect(x, y, w, h, c, f)
             except:
-                super().fill_rect(x, y, w, h, c)   
-        self.register_updates(y, y+h-1)
+                super().fill_rect(x, y, w, h, c)
+        self.register_updates(y, y + h - 1)
+    
+    def ellipse(self, x, y, xr, yr, c, *args, **kwargs):
+        super().ellipse(x, y, xr, yr, c, *args, **kwargs)
+        self.register_updates(y - yr, y + yr)
 
-    # conditionally define optimisations for framebuf extension if loaded 
+    def poly(self, *args, **kwargs):
+        super().poly(*args, **kwargs)
+        self.pages_to_update = (1 << self.pages) - 1
+
+    # conditionally define optimisations for framebuf extension if loaded
     if _fb_variant == 2:
         def large_text(self, s, x, y, m, c=1, r=0, *args, **kwargs):
             try:
                 super().large_text(s, x, y, m, c, r, *args, **kwargs)
             except:
-                raise Exception('extended framebuffer v206 required')
-            h = (8*m) * (1 if r is None or r % 360 // 90 in (0,2) else len(s))
-            self.register_updates(y, y+h-1)
+                raise Exception("extended framebuffer v206+ required")
+            h = (8 * m) * (1 if r is None or r % 360 // 90 in (0, 2) else len(s))
+            self.register_updates(y, y + h - 1)
 
         def circle(self, x, y, radius, c, f:bool = None):
             super().circle(x, y, radius, c, f)
             self.register_updates(y-radius, y+radius)
         
-        def triangle(self, x0, y0, x1, y1, x2, y2, c, f:bool = None):
+        def triangle(self, x0, y0, x1, y1, x2, y2, c, f: bool = None):
             super().triangle(x0, y0, x1, y1, x2, y2, c, f)
             self.register_updates(min(y0, y1, y2), max(y0, y1, y2))
 
     def register_updates(self, y0, y1=None):
-        y1=min((y1 if y1 is not None else y0), self.height-1)
+        y1 = min((y1 if y1 is not None else y0), self.height - 1)
         # this function takes the top and optional bottom address of the changes made
         # and updates the pages_to_change list with any changed pages
         # that are not yet on the list
@@ -342,7 +340,7 @@ class SH1107(framebuf.FrameBuffer):
         if end_page >= 0:
             if start_page < 0:
                 start_page = 0
-            for page in range(start_page, end_page+1):
+            for page in range(start_page, end_page + 1):
                 self.pages_to_update |= 1 << page
 
     def reset(self, res):
@@ -363,12 +361,12 @@ class SH1107_I2C(SH1107):
         if res is not None:
             res.init(res.OUT, value=1)
         super().__init__(width, height, external_vcc, rotate)
-                        
+
     def write_command(self, command_list):
-        self.i2c.writeto(self.address, b'\x00'+command_list)
-        
+        self.i2c.writeto(self.address, b"\x00" + command_list)
+
     def write_data(self, buf):
-        self.i2c.writeto(self.address, b'\x40'+buf)
+        self.i2c.writevto(self.address, (b"\x40", buf))
 
     def reset(self):
         super().reset(self.res)
